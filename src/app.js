e1ea02e41bdfc35d027c3512ec8c16c209b3a7fb
Alexey Grevtsov <grevold@yandex.ru>
2025-08-21 13:15:02 +0500

    add first components

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Title from "./title";
+
+const title = "React with Webpack and Babel";
+
+function App() {
+  return (
+    <div>
+      <Title text={title} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/title.js
@@ -0,0 +1,5 @@
+import React from "react";
+function Title() {
+  return <h1>Заголовок</h1>;
+}
+export default Title;
